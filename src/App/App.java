c7d5ba4895bc5951ae58cd380af2e3818dd8ebd6
WonGi-Kim <kwg970310@gmail.com>
2024-04-22 20:39:39 +0900

    요구사항 2

--- src/App/App.java
+++ src/App/App.java
@@ -14,28 +14,47 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        /** 과제 요구사항 1.
-         * Scanner를 사용하여 양의 정수 2개를 전달 받을 때
-         * 양의 정수는 각각 하나씩 전달 받는다.
-         * 양의 정수는 적합한 타입으로 선언한 변수에 저장한다.
+        /** 과제 요구사항 .2
+         * Scanner를 사용하여 사용하여 사칙연산 기호를 전달받는다.
+         * 사칙연산 기호를 적합한 타입으로 선언된 변수에 저장한다. (charAt(0))
          */
 
         Scanner in = new Scanner(System.in);
 
         int a; // 첫 번째 숫자
         int b; // 두 번째 숫자
+        char operand;
+
         System.out.println("첫 번째 숫자를 입력하세요: ");
         a = in.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        operand = in.next().charAt(0);
+
         System.out.println("두 번째 숫자를 입력하세요: ");
         b = in.nextInt();
 
         System.out.println("\n");
         System.out.println("첫 번째 숫자 : " + a);
+        System.out.println("사칙연산 기호 : " + operand);
         System.out.println("두 번째 숫자 : " + b + " \n");
+
     }
 }
 
 /**
- * 요구사항 .
- *
+ * 과제 요구사항 1.
+ * Scanner를 사용하여 양의 정수 2개를 전달 받을 때
+ * 양의 정수는 각각 하나씩 전달 받는다.
+ * 양의 정수는 적합한 타입으로 선언한 변수에 저장한다.
+ * Scanner in = new Scanner(System.in);
+ * int a; // 첫 번째 숫자
+ * int b; // 두 번째 숫자
+ * System.out.println("첫 번째 숫자를 입력하세요: ");
+ * a = in.nextInt();
+ * System.out.println("두 번째 숫자를 입력하세요: ");
+ * *b = in.nextInt();
+ * *System.out.println("\n");
+ * *System.out.println("첫 번째 숫자 : " + a);
+ * *System.out.println("두 번째 숫자 : " + b + " \n");
  */
