b9ef83b7803f9239890faa5d3f8db6bedfe9aaab
WonGi-Kim <kwg970310@gmail.com>
2024-04-23 20:47:36 +0900

    요구사항 6

--- src/App/App.java
+++ src/App/App.java
@@ -11,60 +11,43 @@
 package App;
 
 import java.util.Scanner;
-import java.util.ArrayList;
-
 
 public class App {
     public static void main(String[] args) {
-        /** 과제 요구사항 .
-         * 연산 결과를 10개를 저장할 수 있는 적합한 타입의 배열을 생성한다.
-         * 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성한다.
-         * 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때 마다 count한다.
-         */
-
         Scanner in = new Scanner(System.in);
-        String finish = new String(); // 연산을 끝내기 위한 문자 변수
+        String finish = new String();
 
         int a; // 첫 번째 숫자
         int b; // 두 번째 숫자
         char operand; // 연산자
-        int result; // 연산 결과
-        int result2; // 나눗셈 나머지 연산 임시 사용
+        int result = 0; // switch 문 밖에서 선언하고 초기화
 
         int[] resultArray = new int[10]; // 연산을 저장할 배열
         int count = 0; // 연산이 저장될 때 마다 count증가
 
-        // while문의 내부 조건은 조건이 만족해야만 반복한다.
         while (!finish.equals("exit")) {
 
-            System.out.println("첫 번째 숫자를 입력하세요: ");
+            System.out.print("첫 번째 숫자를 입력하세요: ");
             a = in.nextInt();
 
-            System.out.println("사칙연산 기호를 입력하세요: ");
+            System.out.print("사칙연산 기호를 입력하세요: ");
             operand = in.next().charAt(0);
 
-            System.out.println("두 번째 숫자를 입력하세요: ");
+            System.out.print("두 번째 숫자를 입력하세요: ");
             b = in.nextInt();
 
-            // Switch 내부에서 count가 증가함
             switch (operand) {
                 case ('+'):
                     result = a + b;
                     System.out.println("결과는 : " + result);
-                    resultArray[count] = result;
-                    count++;
                     break;
                 case ('-'):
                     result = a - b;
                     System.out.println("결과는 : " + result);
-                    resultArray[count] = result;
-                    count++;
                     break;
                 case ('*'):
                     result = a * b;
                     System.out.println("결과는 : " + result);
-                    resultArray[count] = result;
-                    count++;
                     break;
                 case ('/'):
                     if(b == 0) {
@@ -72,16 +55,23 @@ public class App {
                         break;
                     }
                     result = a / b;
-                    result2 = a % b;
+                    int result2 = a % b;
                     System.out.println("결과는 " + "몫 : " + result + " 나머지 : " + result2);
-                    resultArray[count] = result;
-                    count++;
                     break;
 
                 default:
                     System.out.println("잘못된 연산자 입니다.");
                     break;
             }
+
+            if (count > 9) {
+                for (int i = 0; i < resultArray.length - 1; i++) {
+                    resultArray[i] = resultArray[i + 1];
+                }
+                count = 9;
+            }
+            resultArray[count] = result;
+            count++;
 
             System.out.print("더 계산하시겠습니까? ( 계속 계산하시려면 아무키나 입력해주세요. exit 입력 시 종료됩니다. )");
             finish = in.next();
@@ -95,9 +85,9 @@ public class App {
         for (int i = 0; i < resultArray.length; i++) {
             System.out.print(resultArray[i] + " "); // 배열에 값이 정상적으로 저장 되었는지
         }
-
     }
 }
+
 
 /**
  * 강의를 들으면서 while문 안쓰던게 뽀록남....
@@ -108,4 +98,3 @@ public class App {
  *
  * 내가 한 설명 두 개 합친거;;
  */
-
