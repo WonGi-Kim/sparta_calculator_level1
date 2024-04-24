c5a81c39d39ac91814c3134ae4762f64d047f514
WonGi-Kim <kwg970310@gmail.com>
2024-04-24 16:34:17 +0900

    요구사항 7

--- src/App/App.java
+++ src/App/App.java
@@ -10,20 +10,26 @@
 
 package App;
 
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Queue;
 
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String finish = new String();
+        String stopQueue = new String();
 
         int a; // 첫 번째 숫자
         int b; // 두 번째 숫자
         char operand; // 연산자
         int result = 0; // switch 문 밖에서 선언하고 초기화
 
-        int[] resultArray = new int[10]; // 연산을 저장할 배열
-        int count = 0; // 연산이 저장될 때 마다 count증가
+        // int[] resultArray = new int[10]; // 연산을 저장할 배열
+        // int count = 0; // 연산이 저장될 때 마다 count증가 -> 배열을 Queue로 바꾸고 사용 X
+
+        // 요구사항 7번 무한이 저장되는 컬렉션 프레임 사용
+        Queue<Integer> resultQueue = new LinkedList<>();
 
         while (!finish.equals("exit")) {
 
@@ -58,22 +64,22 @@ public class App {
                     int result2 = a % b;
                     System.out.println("결과는 " + "몫 : " + result + " 나머지 : " + result2);
                     break;
-
                 default:
                     System.out.println("잘못된 연산자 입니다.");
                     break;
             }
 
-            if (count > 9) {
-                for (int i = 0; i < resultArray.length - 1; i++) {
-                    resultArray[i] = resultArray[i + 1];
+            // 요구사항 7 Queue에서 remove를 사용하면 가장 먼저 저장된 결과 삭제
+            resultQueue.offer(result);
+            if(resultQueue.size() >= 2) { // remove는 큐가 비어있는 경우 예외가 발생하므로 예외 처리를 먼저 해줘야 함
+                System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까?");
+                stopQueue = in.next();
+                if (stopQueue.equals("remove")) {
+                    resultQueue.remove();
                 }
-                count = 9;
             }
-            resultArray[count] = result;
-            count++;
 
-            System.out.print("더 계산하시겠습니까? ( 계속 계산하시려면 아무키나 입력해주세요. exit 입력 시 종료됩니다. )");
+            System.out.print("더 계산하시겠습니까? (계속 계산하시려면 아무키나 입력해주세요. exit 입력 시 종료됩니다.)");
             finish = in.next();
             if(finish.equals("exit")) {
                 break;
@@ -81,20 +87,20 @@ public class App {
                 continue;
             }
         }
-        System.out.println(count); // count가 증가하는지
-        for (int i = 0; i < resultArray.length; i++) {
-            System.out.print(resultArray[i] + " "); // 배열에 값이 정상적으로 저장 되었는지
-        }
     }
 }
 
 
 /**
- * 강의를 들으면서 while문 안쓰던게 뽀록남....
- * 뭐랄까... 쓸 줄은 아는데 설명은 못하는..?
- *
- * while(조건) 조건을 만족해야 반복
- * for(변수;연산;증감;) 연산이 만족할 때 까지 반복
- *
- * 내가 한 설명 두 개 합친거;;
+ * 요구사항 6번 하면서 이 부분이 제일 힘들었다..
+ * 진짜 별거 아니었는데 결과를 저장하는 곳을 변경해야겠다는 생각을 너무 늦게 했다.
+ * -> 계속 10번째 입력부터 값이 한 번 더 일찍 밀린 이유 때문에
+ if (count > 9) {
+ for (int i = 0; i < resultArray.length - 1; i++) {
+ resultArray[i] = resultArray[i + 1];
+ }
+ count = 9;
+ }
+ resultArray[count] = result;
+ count++;
  */
